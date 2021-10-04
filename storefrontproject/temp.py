42593399759a7546c3c87d7983bd97fd6d36abcb
tuannguyen_ <44093611+SledgyNguyen@users.noreply.github.com>
2021-10-04 22:00:06 +0200

    testing pylint

--- storefrontproject/temp.py
+++ storefrontproject/temp.py
@@ -1,6 +1,1 @@
-from pathlib import Path
-import os
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-print(os.path.join(BASE_DIR, "storefrontproject"))
+print("Hello World")
