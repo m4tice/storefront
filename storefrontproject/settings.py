4fa2edf691273a970ac52371a20040e82677a675
tuannguyen_ <44093611+SledgyNguyen@users.noreply.github.com>
2021-10-04 21:56:53 +0200

    temp

--- storefrontproject/settings.py
+++ storefrontproject/settings.py
@@ -11,10 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.join(Path(__file__).resolve().parent.parent, 'storefrontproject')
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
